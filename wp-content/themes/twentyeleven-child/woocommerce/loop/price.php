968fee094c475bd4d98b7506afe7c2aad6133df9
mani-agiratech <manikandan@agiratech.com>
2017-02-28 18:40:46 +0530

    WP CUS - Product add/Edit Changes

--- wp-content/themes/twentyeleven-child/woocommerce/loop/price.php
+++ wp-content/themes/twentyeleven-child/woocommerce/loop/price.php
@@ -11,7 +11,15 @@ if ( ! defined( 'ABSPATH' ) ) exit; // Exit if accessed directly
 
 global $product;
 ?>
-
+<?php
+if($product->get_type() == "course")
+{
+  $device_limit = ($product->get_devices_limit() == ''?"Unlimited":$product->get_devices_limit());
+  $token_expiry = ($product->get_token_expiry() == ''?"Never":$product->get_token_expiry()).'(Days)';
+  echo '<span class=" price"> Devices : '.$device_limit.'</span>';
+  echo '<span class=" price">Expiry : '.$token_expiry.'</span>';
+}
+?>
 <?php if ($price_html = $product->get_price_html()) : ?>
 	<span class="price"><?php echo $price_html; ?>
 	<?php
